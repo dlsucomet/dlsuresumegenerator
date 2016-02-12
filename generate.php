06e273e12173ef3f38c9eab45dbea349204894fe
Richard Parayno <broodheart@gmail.com>
2016-02-13 02:38:34 +0800

    generator implementation 1
    php implementation, VERY SLOW

---
+++ generate.php
@@ -0,0 +1,25 @@
+<?php
+// this implementation takes 3 MINUTES to process file and pop up the download dialog (using a styled landing page)
+ 
+// include autoloader
+require_once 'dompdf/autoload.inc.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$file = file_get_contents('index.html');
+$dompdf = new Dompdf();
+$dompdf->set_option('isHtml5ParserEnabled', true);
+$dompdf->loadHtml($file);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portrait');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+
+?>
