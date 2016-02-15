1aea30e4f685d771ed9e0fe99e66d70db1c5eae9
Richard Parayno <broodheart@gmail.com>
2016-02-15 17:47:50 +0800

    made some edits to user stories and php file

--- generate.php
+++ generate.php
@@ -14,7 +14,7 @@ $dompdf->set_option('isHtml5ParserEnabled', true);
 $dompdf->loadHtml($file);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'portrait');
+$dompdf->setPaper('A4', 'landscape');
 
 // Render the HTML as PDF
 $dompdf->render();
