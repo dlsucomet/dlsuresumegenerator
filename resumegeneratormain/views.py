e873198c89a0c80f015575cfabc68e1563973380
Richard Parayno <broodheart@gmail.com>
2017-10-18 13:48:00 +0800

    removed unused reference to weasyprint in views.py

--- resumegeneratormain/views.py
+++ resumegeneratormain/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from weasyprint import HTML, CSS
 from django.contrib import messages
 from django.template import loader
 
